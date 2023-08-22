c8f42d11630228580b86d1d78c408d3a2142d754
aman githinji <amangithinji09@gmail.com>
2023-08-22 21:58:28 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        https://open.spotify.com/track/4TxaDMTivAvGYVDV1BTNu3?si=b4a62de2387e403b
+      </p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  );
 }
 
 export default About;
